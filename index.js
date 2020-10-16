33159778a08be105af567ea0529ae53d4fafdbfd
firyafatur <faturgames19@gmail.com>
2020-10-16 09:33:09 +0700

    Part 1 - Membuat server Express

---
+++ index.js
@@ -0,0 +1,10 @@
+const exprees = require("express");
+const app = exprees();
+
+app.get("/", function (req, res) {
+  return res.send("Hello world!");
+});
+
+app.listen(3000, function () {
+  console.log("Server running at port 3000");
+});
